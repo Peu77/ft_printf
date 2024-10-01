a0641b808b112dc713d6d144de1ec8d2e500198f
Emil Ebert <emilebert@icloud.com>
2024-10-01 14:17:43 +0200

    add precision for strings

--- src/print_string.c
+++ src/print_string.c
@@ -2,5 +2,14 @@
 #include "libft.h"
 
 const char *print_string(t_flags *flags, va_list *args) {
-    return ft_strdup(va_arg(*args, char *));
+    char *str = va_arg(*args, char *);
+
+    if (!str) {
+        if (flags->dot && flags->precision < 6)
+            return ft_strdup("");
+        return ft_strdup("(null)");
+    }
+    if (flags->dot && flags->precision < ft_strlen(str))
+        return ft_substr(str, 0, flags->precision);
+    return ft_strdup(str);
 }
